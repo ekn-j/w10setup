6f6e5db4dc270825fb75877fb30f2baf0d818cca
Johannes Ek <johannes3k@gmail.com>
2020-09-27 13:28:42 +0200

    First commit

---
+++ chocosetup.ps1
@@ -0,0 +1,4 @@
+ECHO Lets install chocolatey! 
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
